1ef4987cf4feb8d3c054af2494419e2b89890786
rqysir609 <rqysir609@gmail.com>
2026-04-28 19:04:51 +0800

    Initial commit

--- Assets/XFramework/Runtime/Core/EntityNode.cs
+++ Assets/XFramework/Runtime/Core/EntityNode.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 
 namespace XFramework
 {
@@ -10,25 +11,40 @@ namespace XFramework
     public interface IEntityNode : IParentNode
     {
         /// <summary>获取指定类型的子节点。优先从缓存中查找，未找到时可根据参数自动创建。</summary>
-        T GetComponent<T>(bool autoCreate = true) where T : IBaseNode;
+        T GetNode<T>(bool autoCreate = true) where T : IBaseNode;
 
         /// <summary>添加指定类型的子节点。如果已存在同类型节点则直接返回。</summary>
-        T AddComponent<T>() where T : BaseNode, new();
+        T AddNode<T>() where T : BaseNode, new();
 
         /// <summary>添加指定类型的子节点，并传入初始化参数。如果已存在同类型节点则直接返回。</summary>
-        T AddComponent<T>(object arg) where T : BaseNode, new();
+        T AddNode<T>(object arg) where T : BaseNode, new();
 
         /// <summary>通过运行时类型添加子节点。如果已存在同类型节点则直接返回。</summary>
-        BaseNode AddComponent(Type type);
+        BaseNode AddNode(Type type);
 
         /// <summary>通过运行时类型添加子节点，并传入初始化参数。如果已存在同类型节点则直接返回。</summary>
-        BaseNode AddComponent(Type type, object arg);
+        BaseNode AddNode(Type type, object arg);
+
+        /// <summary>添加一个子节点，并立即执行异步启动。</summary>
+        UniTask<T> AddNodeAsync<T>() where T : BaseNode, IParentNode, new();
+
+        /// <summary>添加一个子节点（带参数），并立即执行异步启动。</summary>
+        UniTask<T> AddNodeAsync<T>(object arg) where T : BaseNode, IParentNode, new();
+
+        /// <summary>通过运行时类型添加一个子节点，并立即执行异步启动。</summary>
+        UniTask<BaseNode> AddNodeAsync(Type type);
+
+        /// <summary>通过运行时类型添加一个子节点（带参数），并立即执行异步启动。</summary>
+        UniTask<BaseNode> AddNodeAsync(Type type, object arg);
 
         /// <summary>移除指定类型的子节点。</summary>
-        bool RemoveComponent<T>() where T : IBaseNode;
+        bool RemoveNode<T>() where T : IBaseNode;
+
+        /// <summary>通过运行时类型移除子节点。</summary>
+        bool RemoveNode(Type type);
 
         /// <summary>移除指定的子节点实例。</summary>
-        bool RemoveComponent(BaseNode node);
+        bool RemoveNode(BaseNode node);
     }
 
     /// <summary>
@@ -56,7 +72,7 @@ namespace XFramework
         /// <param name="autoCreate">未找到时是否自动创建。默认为 true。
         /// <para>注意：当 T 为接口类型时，无法自动创建，此参数无效。</para></param>
         /// <returns>匹配的子节点，未找到且无法自动创建时返回 null。</returns>
-        public T GetComponent<T>(bool autoCreate = true) where T : IBaseNode
+        public T GetNode<T>(bool autoCreate = true) where T : IBaseNode
         {
             Type type = typeof(T);
 
@@ -89,9 +105,9 @@ namespace XFramework
         /// 添加指定类型的子节点。如果已存在同类型节点则直接返回。
         /// <para>节点通过 <see cref="NodeFactory"/> 获取，支持缓存池复用。</para>
         /// </summary>
-        /// <typeparam name="T">要创建的子节点类型，必须有无参构造函数。</typeparam>
-        /// <returns>创建或已存在的子节点。</returns>
-        public T AddComponent<T>() where T : BaseNode, new()
+        /// <typeparam name="T">要添加的子节点类型，必须有无参构造函数。</typeparam>
+        /// <returns>已添加或已存在的子节点。</returns>
+        public T AddNode<T>() where T : BaseNode, new()
         {
             if (_typeCache.TryGetValue(typeof(T), out BaseNode node))
                 return (T)node;
@@ -106,10 +122,10 @@ namespace XFramework
         /// 添加指定类型的子节点，并传入初始化参数。如果已存在同类型节点则直接返回。
         /// <para>节点通过 <see cref="NodeFactory"/> 获取，支持缓存池复用。</para>
         /// </summary>
-        /// <typeparam name="T">要创建的子节点类型，必须有无参构造函数。</typeparam>
+        /// <typeparam name="T">要添加的子节点类型，必须有无参构造函数。</typeparam>
         /// <param name="arg">初始化参数。</param>
-        /// <returns>创建或已存在的子节点。</returns>
-        public T AddComponent<T>(object arg) where T : BaseNode, new()
+        /// <returns>已添加或已存在的子节点。</returns>
+        public T AddNode<T>(object arg) where T : BaseNode, new()
         {
             if (_typeCache.TryGetValue(typeof(T), out BaseNode node))
                 return (T)node;
@@ -121,11 +137,142 @@ namespace XFramework
         }
 
         /// <summary>
+        /// 通过运行时类型添加子节点。如果已存在同类型节点则直接返回。
+        /// <para>节点通过 <see cref="NodeFactory"/> 获取，支持缓存池复用。</para>
+        /// </summary>
+        /// <param name="type">要添加的子节点类型，必须是 <see cref="BaseNode"/> 的子类。</param>
+        /// <returns>已添加或已存在的子节点。</returns>
+        /// <exception cref="ArgumentNullException">type 为 null。</exception>
+        /// <exception cref="ArgumentException">type 不是 BaseNode 的子类。</exception>
+        public BaseNode AddNode(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            if (!typeof(BaseNode).IsAssignableFrom(type))
+                throw new ArgumentException($"Add {type} failed, it is not a BaseNode");
+
+            if (_typeCache.TryGetValue(type, out BaseNode node))
+                return node;
+
+            node = NodeFactory.GetNode(type);
+            _typeCache[type] = node;
+            AddChild(node);
+            return node;
+        }
+
+        /// <summary>
+        /// 通过运行时类型添加子节点，并传入初始化参数。如果已存在同类型节点则直接返回。
+        /// <para>节点通过 <see cref="NodeFactory"/> 获取，支持缓存池复用。</para>
+        /// </summary>
+        /// <param name="type">要添加的子节点类型，必须是 <see cref="BaseNode"/> 的子类。</param>
+        /// <param name="arg">初始化参数。</param>
+        /// <returns>已添加或已存在的子节点。</returns>
+        /// <exception cref="ArgumentNullException">type 为 null。</exception>
+        /// <exception cref="ArgumentException">type 不是 BaseNode 的子类。</exception>
+        public BaseNode AddNode(Type type, object arg)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            if (!typeof(BaseNode).IsAssignableFrom(type))
+                throw new ArgumentException($"Add {type} failed, it is not a BaseNode");
+
+            if (_typeCache.TryGetValue(type, out BaseNode node))
+                return node;
+
+            node = NodeFactory.GetNode(type, arg);
+            _typeCache[type] = node;
+            AddChild(node);
+            return node;
+        }
+
+        /// <summary>
+        /// 添加一个子节点，并立即执行异步启动。
+        /// <para>节点会先挂入树（此时可访问父节点上的服务），再执行异步加载管线，
+        /// 加载完成后自动触发 <see cref="BaseNode.Start"/>。</para>
+        /// </summary>
+        /// <typeparam name="T">子节点类型，必须有无参构造函数且实现 <see cref="IParentNode"/>。</typeparam>
+        /// <returns>已添加并完成异步启动的子节点实例。</returns>
+        public async UniTask<T> AddNodeAsync<T>() where T : BaseNode, IParentNode, new()
+        {
+            T node = NodeFactory.GetNode<T>();
+            AddChild(node, deferStart: true);
+            await ((IParentNode)node).StartupAsync();
+            return node;
+        }
+
+        /// <summary>
+        /// 添加一个子节点（带参数），并立即执行异步启动。
+        /// </summary>
+        /// <typeparam name="T">子节点类型，必须有无参构造函数且实现 <see cref="IParentNode"/>。</typeparam>
+        /// <param name="arg">初始化参数。</param>
+        /// <returns>已添加并完成异步启动的子节点实例。</returns>
+        public async UniTask<T> AddNodeAsync<T>(object arg) where T : BaseNode, IParentNode, new()
+        {
+            T node = NodeFactory.GetNode<T>(arg);
+            AddChild(node, deferStart: true);
+            await ((IParentNode)node).StartupAsync();
+            return node;
+        }
+
+        /// <summary>
+        /// 通过运行时类型添加一个子节点，并立即执行异步启动。
+        /// <para>节点会先挂入树（此时可访问父节点上的服务），再执行异步加载管线，
+        /// 加载完成后自动触发 <see cref="BaseNode.Start"/>。</para>
+        /// </summary>
+        /// <param name="type">子节点类型，必须是 <see cref="BaseNode"/> 的子类且实现 <see cref="IParentNode"/>。</param>
+        /// <returns>已添加并完成异步启动的子节点实例。</returns>
+        /// <exception cref="ArgumentNullException">type 为 null。</exception>
+        /// <exception cref="ArgumentException">type 不是 BaseNode 的子类或未实现 IParentNode。</exception>
+        public async UniTask<BaseNode> AddNodeAsync(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            if (!typeof(BaseNode).IsAssignableFrom(type))
+                throw new ArgumentException($"AddNodeAsync {type} failed, it is not a BaseNode");
+
+            if (!typeof(IParentNode).IsAssignableFrom(type))
+                throw new ArgumentException($"AddNodeAsync {type} failed, it does not implement IParentNode");
+
+            BaseNode node = NodeFactory.GetNode(type);
+            AddChild(node, deferStart: true);
+            await ((IParentNode)node).StartupAsync();
+            return node;
+        }
+
+        /// <summary>
+        /// 通过运行时类型添加一个子节点（带参数），并立即执行异步启动。
+        /// </summary>
+        /// <param name="type">子节点类型，必须是 <see cref="BaseNode"/> 的子类且实现 <see cref="IParentNode"/>。</param>
+        /// <param name="arg">初始化参数。</param>
+        /// <returns>已添加并完成异步启动的子节点实例。</returns>
+        /// <exception cref="ArgumentNullException">type 为 null。</exception>
+        /// <exception cref="ArgumentException">type 不是 BaseNode 的子类或未实现 IParentNode。</exception>
+        public async UniTask<BaseNode> AddNodeAsync(Type type, object arg)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            if (!typeof(BaseNode).IsAssignableFrom(type))
+                throw new ArgumentException($"AddNodeAsync {type} failed, it is not a BaseNode");
+
+            if (!typeof(IParentNode).IsAssignableFrom(type))
+                throw new ArgumentException($"AddNodeAsync {type} failed, it does not implement IParentNode");
+
+            BaseNode node = NodeFactory.GetNode(type, arg);
+            AddChild(node, deferStart: true);
+            await ((IParentNode)node).StartupAsync();
+            return node;
+        }
+
+        /// <summary>
         /// 移除指定类型的子节点。
         /// </summary>
         /// <typeparam name="T">要移除的子节点类型。</typeparam>
         /// <returns>是否成功移除。</returns>
-        public bool RemoveComponent<T>() where T : IBaseNode
+        public bool RemoveNode<T>() where T : IBaseNode
         {
             Type type = typeof(T);
 
@@ -157,54 +304,39 @@ namespace XFramework
         }
 
         /// <summary>
-        /// 通过运行时类型添加子节点。如果已存在同类型节点则直接返回。
-        /// <para>节点通过 <see cref="NodeFactory"/> 获取，支持缓存池复用。</para>
+        /// 通过运行时类型移除子节点。
         /// </summary>
-        /// <param name="type">要创建的子节点类型，必须是 <see cref="BaseNode"/> 的子类。</param>
-        /// <returns>创建或已存在的子节点。</returns>
-        /// <exception cref="ArgumentNullException">type 为 null。</exception>
-        /// <exception cref="ArgumentException">type 不是 BaseNode 的子类。</exception>
-        public BaseNode AddComponent(Type type)
+        /// <param name="type">要移除的子节点类型。</param>
+        /// <returns>是否成功移除。</returns>
+        public bool RemoveNode(Type type)
         {
-            if (type == null)
-                throw new ArgumentNullException(nameof(type));
+            if (type == null) return false;
 
-            if (!typeof(BaseNode).IsAssignableFrom(type))
-                throw new ArgumentException($"Add {type} failed, it is not a BaseNode");
+            if (type.IsInterface)
+            {
+                if (_interfaceCache.TryGetValue(type, out var cached))
+                {
+                    _interfaceCache.Remove(type);
 
-            if (_typeCache.TryGetValue(type, out BaseNode node))
-                return node;
-
-            node = NodeFactory.GetNode(type);
-            _typeCache[type] = node;
-            AddChild(node);
-            return node;
-        }
-
-        /// <summary>
-        /// 通过运行时类型添加子节点，并传入初始化参数。如果已存在同类型节点则直接返回。
-        /// <para>节点通过 <see cref="NodeFactory"/> 获取，支持缓存池复用。</para>
-        /// </summary>
-        /// <param name="type">要创建的子节点类型，必须是 <see cref="BaseNode"/> 的子类。</param>
-        /// <param name="arg">初始化参数。</param>
-        /// <returns>创建或已存在的子节点。</returns>
-        /// <exception cref="ArgumentNullException">type 为 null。</exception>
-        /// <exception cref="ArgumentException">type 不是 BaseNode 的子类。</exception>
-        public BaseNode AddComponent(Type type, object arg)
-        {
-            if (type == null)
-                throw new ArgumentNullException(nameof(type));
-
-            if (!typeof(BaseNode).IsAssignableFrom(type))
-                throw new ArgumentException($"Add {type} failed, it is not a BaseNode");
-
-            if (_typeCache.TryGetValue(type, out BaseNode node))
-                return node;
-
-            node = NodeFactory.GetNode(type, arg);
-            _typeCache[type] = node;
-            AddChild(node);
-            return node;
+                    var node = cached as BaseNode;
+                    if (node == null) return false;
+                    RemoveFromAllCaches(node);
+                    RemoveChild(node);
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                if (_typeCache.TryGetValue(type, out BaseNode node))
+                {
+                    _typeCache.Remove(type);
+                    RemoveFromAllCaches(node);
+                    RemoveChild(node);
+                    return true;
+                }
+                return false;
+            }
         }
 
         /// <summary>
@@ -212,7 +344,7 @@ namespace XFramework
         /// </summary>
         /// <param name="node">要移除的子节点。</param>
         /// <returns>是否成功移除。</returns>
-        public bool RemoveComponent(BaseNode node)
+        public bool RemoveNode(BaseNode node)
         {
             if (node == null) return false;
 
--- Assets/XFramework/Runtime/Core/RootNode.cs
+++ Assets/XFramework/Runtime/Core/RootNode.cs
@@ -1,24 +1,11 @@
 using System;
-using Cysharp.Threading.Tasks;
 
 namespace XFramework
 {
     /// <summary>
-    /// 根节点接口。作为节点树的入口，提供便捷的节点创建方法。
-    /// </summary>
-    public interface IRootNode : IEntityNode
-    {
-        /// <summary>创建并添加一个子节点。</summary>
-        T CreateNode<T>() where T : BaseNode, new();
-
-        /// <summary>创建并添加一个子节点，并传入初始化参数。</summary>
-        T CreateNode<T>(object arg) where T : BaseNode, new();
-    }
-
-    /// <summary>
     /// 根节点。作为节点树的入口，提供便捷的节点创建方法。
     /// </summary>
-    public class RootNode : EntityNode, IRootNode
+    public class RootNode : EntityNode
     {
         #region Static Methods
 
@@ -45,63 +32,6 @@ namespace XFramework
             RootNode root = NodeFactory.GetNode<RootNode>(arg);
             root.Awake();
             return root;
-        }
-
-        #endregion
-
-        #region Public Methods
-
-        /// <summary>
-        /// 创建并添加一个子节点。
-        /// <para>通过 <see cref="NodeFactory"/> 获取节点实例，自动添加到当前根节点并完成初始化。</para>
-        /// <para>如果同类型节点已存在，则直接返回已有节点。</para>
-        /// </summary>
-        /// <typeparam name="T">子节点类型，必须有无参构造函数。</typeparam>
-        /// <returns>已添加的子节点实例。</returns>
-        public T CreateNode<T>() where T : BaseNode, new()
-        {
-            return AddComponent<T>();
-        }
-
-        /// <summary>
-        /// 创建并添加一个子节点，并传入初始化参数。
-        /// <para>如果同类型节点已存在，则直接返回已有节点。</para>
-        /// </summary>
-        /// <typeparam name="T">子节点类型，必须有无参构造函数。</typeparam>
-        /// <param name="arg">初始化参数。</param>
-        /// <returns>已添加的子节点实例。</returns>
-        public T CreateNode<T>(object arg) where T : BaseNode, new()
-        {
-            return AddComponent<T>(arg);
-        }
-
-        /// <summary>
-        /// 创建并添加一个子节点，并立即执行异步启动。
-        /// <para>节点会先挂入树（此时可访问 RootNode 上的服务），再执行异步加载管线，
-        /// 加载完成后自动触发 <see cref="BaseNode.Start"/>。</para>
-        /// </summary>
-        /// <typeparam name="T">子节点类型，必须有无参构造函数且实现 <see cref="IParentNode"/>。</typeparam>
-        /// <returns>已添加并完成异步启动的子节点实例。</returns>
-        public async UniTask<T> CreateNodeAsync<T>() where T : BaseNode, IParentNode, new()
-        {
-            T node = NodeFactory.GetNode<T>();
-            AddChild(node, deferStart: true);
-            await ((IParentNode)node).StartupAsync();
-            return node;
-        }
-
-        /// <summary>
-        /// 创建并添加一个子节点（带参数），并立即执行异步启动。
-        /// </summary>
-        /// <typeparam name="T">子节点类型，必须有无参构造函数且实现 <see cref="IParentNode"/>。</typeparam>
-        /// <param name="arg">初始化参数。</param>
-        /// <returns>已添加并完成异步启动的子节点实例。</returns>
-        public async UniTask<T> CreateNodeAsync<T>(object arg) where T : BaseNode, IParentNode, new()
-        {
-            T node = NodeFactory.GetNode<T>(arg);
-            AddChild(node, deferStart: true);
-            await ((IParentNode)node).StartupAsync();
-            return node;
         }
 
         #endregion
