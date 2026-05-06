cdc036b4a9314d729f36c4fa052aa3d8360b29aa
rqysir609 <rqysir609@gmail.com>
2026-05-06 22:01:56 +0800

    添加R3封装

--- Assets/XFramework/Runtime/Reactive/ReactiveProperty.cs
+++ Assets/XFramework/Runtime/Reactive/ReactiveProperty.cs
@@ -4,27 +4,73 @@ using R3;
 namespace XFramework
 {
     /// <summary>
-    /// 基于 R3 ReactiveProperty 的响应式属性实现。
+    /// 响应式属性节点。包含一个可自动推送变化的响应式值。
+    /// <para>适用于血量、分数、状态等需要被监听的属性。</para>
+    /// <para>节点本身实现了 <see cref="IReactiveProperty{T}"/>，可直接通过 <see cref="Subscribe"/> 监听值变化。</para>
     /// </summary>
-    internal sealed class ReactiveProperty<T> : IReactiveProperty<T>
+    /// <typeparam name="T">值的类型。</typeparam>
+    public class ReactiveProperty<T> : LeafNode, IReactiveProperty<T>
     {
-        private readonly R3.ReactiveProperty<T> _property;
+        #region Private Fields
 
+        private R3.ReactiveProperty<T> _value;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>获取或设置值。设置时自动通知所有订阅者。</summary>
         public T Value
         {
-            get => _property.Value;
-            internal set => _property.Value = value;
+            get => _value != null ? _value.Value : default;
+            set
+            {
+                if (_value != null)
+                {
+                    _value.Value = value;
+                }
+            }
         }
 
-        public ReactiveProperty(T initialValue)
-        {
-            _property = new R3.ReactiveProperty<T>(initialValue);
-        }
+        #endregion
 
+        #region IReadonlySignal
+
+        /// <summary>订阅值变化。节点销毁时自动取消订阅。</summary>
         public IDisposable Subscribe(Action<T> onNext)
-            => _property.Subscribe(onNext);
+            => _value?.Subscribe(onNext);
 
-        public void Dispose()
-            => _property.Dispose();
+        #endregion
+
+        #region Lifecycle Overrides
+
+        protected override void OnAwake()
+        {
+            _value ??= new R3.ReactiveProperty<T>(default);
+        }
+
+        protected override void OnDestroy()
+        {
+            _value?.Dispose();
+            _value = null;
+        }
+
+        protected override void OnInit(object arg)
+        {
+            if (arg is T initialValue)
+            {
+                if (_value != null)
+                {
+                    _value.Dispose();
+                    _value = new R3.ReactiveProperty<T>(initialValue);
+                }
+                else
+                {
+                    _value = new R3.ReactiveProperty<T>(initialValue);
+                }
+            }
+        }
+
+        #endregion
     }
 }
