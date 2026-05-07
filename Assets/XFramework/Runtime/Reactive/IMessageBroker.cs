cfd90c444ab331aa237aad02cc7183f6de77d93f
rqysir609 <rqysir609@gmail.com>
2026-05-07 21:37:30 +0800

    添加R3封装

--- Assets/XFramework/Runtime/Reactive/IMessageBroker.cs
+++ Assets/XFramework/Runtime/Reactive/IMessageBroker.cs
@@ -1,45 +1,21 @@
 using System;
-using Cysharp.Threading.Tasks;
 
 namespace XFramework
 {
     /// <summary>
-    /// 消息发布器。按类型推送消息。
+    /// 消息发布器标记接口。
+    /// <para>节点实现此接口后，可通过 <see cref="MessageBus"/> 的扩展方法发布消息。</para>
     /// </summary>
     public interface IMessagePublisher
     {
-        /// <summary>发布指定类型的消息。</summary>
-        void Publish<TMessage>(TMessage message);
-
-        /// <summary>发布带键值的消息。相同 Key 的消息在同一通道中传递。</summary>
-        void Publish<TKey, TMessage>(TKey key, TMessage message);
     }
 
     /// <summary>
-    /// 消息订阅器。按类型订阅消息。
+    /// 消息订阅器标记接口。
+    /// <para>节点实现此接口后，可通过 <see cref="MessageBus"/> 的扩展方法订阅消息。</para>
     /// </summary>
     public interface IMessageSubscriber
     {
-        /// <summary>订阅指定类型的消息。</summary>
-        IReadonlySignal<TMessage> Subscribe<TMessage>();
-
-        /// <summary>订阅指定类型的消息，并附加过滤条件。</summary>
-        IReadonlySignal<TMessage> Subscribe<TMessage>(Predicate<TMessage> filter);
-
-        /// <summary>订阅指定键值的消息。</summary>
-        IReadonlySignal<TMessage> Subscribe<TKey, TMessage>(TKey key);
-
-        /// <summary>异步订阅。消息到达时执行异步处理器。</summary>
-        IReadonlySignal<TMessage> SubscribeAsync<TMessage>(Func<TMessage, UniTask> asyncHandler);
-
-        /// <summary>异步订阅，并附加过滤条件。</summary>
-        IReadonlySignal<TMessage> SubscribeAsync<TMessage>(Predicate<TMessage> filter, Func<TMessage, UniTask> asyncHandler);
-
-        /// <summary>订阅带缓冲的消息。新订阅者会立即收到最近一次发布的消息。</summary>
-        IReadonlySignal<TMessage> SubscribeBuffered<TMessage>();
-
-        /// <summary>订阅带缓冲的键值消息。新订阅者会立即收到最近一次发布的消息。</summary>
-        IReadonlySignal<TMessage> SubscribeBuffered<TKey, TMessage>(TKey key);
     }
 
     /// <summary>
---
+++ Assets/XFramework/Runtime/Reactive/MessageBus.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+
+namespace XFramework
+{
+    /// <summary>
+    /// 全局消息总线。提供静态 API 和节点扩展方法两种使用方式。
+    /// <para>静态 API：非节点类可通过 <see cref="MessageBus"/> 直接发布/订阅消息。</para>
+    /// <para>扩展方法：节点实现 <see cref="IMessagePublisher"/> / <see cref="IMessageSubscriber"/> 后，
+    /// 可通过 <c>this.Publish()</c> / <c>this.Subscribe()</c> 使用，订阅会自动绑定节点生命周期。</para>
+    /// </summary>
+    public static class MessageBus
+    {
+        #region Private Fields
+
+        private static MessageBroker _broker = new MessageBroker();
+
+        #endregion
+
+        #region Static API
+
+        /// <summary>发布指定类型的消息。</summary>
+        public static void Publish<TMessage>(TMessage message) => _broker.Publish(message);
+
+        /// <summary>发布带键值的消息。相同 Key 的消息在同一通道中传递。</summary>
+        public static void Publish<TKey, TMessage>(TKey key, TMessage message) => _broker.Publish(key, message);
+
+        /// <summary>订阅指定类型的消息。</summary>
+        public static IDisposable Subscribe<TMessage>(Action<TMessage> handler)
+            => _broker.Subscribe<TMessage>().Subscribe(handler);
+
+        /// <summary>订阅指定类型的消息，并附加过滤条件。</summary>
+        public static IDisposable Subscribe<TMessage>(Predicate<TMessage> filter, Action<TMessage> handler)
+            => _broker.Subscribe(filter).Subscribe(handler);
+
+        /// <summary>订阅指定键值的消息。</summary>
+        public static IDisposable Subscribe<TKey, TMessage>(TKey key, Action<TMessage> handler)
+            => _broker.Subscribe<TKey, TMessage>(key).Subscribe(handler);
+
+        /// <summary>异步订阅。消息到达时执行异步处理器。</summary>
+        public static IDisposable SubscribeAsync<TMessage>(Func<TMessage, UniTask> asyncHandler)
+            => _broker.SubscribeAsync(asyncHandler).Subscribe(_ => { });
+
+        /// <summary>异步订阅，并附加过滤条件。</summary>
+        public static IDisposable SubscribeAsync<TMessage>(Predicate<TMessage> filter, Func<TMessage, UniTask> asyncHandler)
+            => _broker.SubscribeAsync(filter, asyncHandler).Subscribe(_ => { });
+
+        /// <summary>订阅带缓冲的消息。新订阅者会立即收到最近一次发布的消息。</summary>
+        public static IDisposable SubscribeBuffered<TMessage>(Action<TMessage> handler)
+            => _broker.SubscribeBuffered<TMessage>().Subscribe(handler);
+
+        /// <summary>订阅带缓冲的键值消息。新订阅者会立即收到最近一次发布的消息。</summary>
+        public static IDisposable SubscribeBuffered<TKey, TMessage>(TKey key, Action<TMessage> handler)
+            => _broker.SubscribeBuffered<TKey, TMessage>(key).Subscribe(handler);
+
+        /// <summary>清理所有订阅和缓存。</summary>
+        public static void Clear()
+        {
+            _broker.Clear();
+            _broker = new MessageBroker();
+        }
+
+        #endregion
+
+        #region Extension Methods (for IMessagePublisher)
+
+        /// <summary>发布指定类型的消息。</summary>
+        public static void Publish<TMessage>(this IMessagePublisher publisher, TMessage message)
+            => _broker.Publish(message);
+
+        /// <summary>发布带键值的消息。相同 Key 的消息在同一通道中传递。</summary>
+        public static void Publish<TKey, TMessage>(this IMessagePublisher publisher, TKey key, TMessage message)
+            => _broker.Publish(key, message);
+
+        #endregion
+
+        #region Extension Methods (for IMessageSubscriber)
+
+        /// <summary>订阅指定类型的消息。订阅会自动绑定到节点的生命周期，节点销毁时自动取消。</summary>
+        public static IDisposable Subscribe<TMessage>(this IMessageSubscriber subscriber, Action<TMessage> handler)
+        {
+            var disposable = _broker.Subscribe<TMessage>().Subscribe(handler);
+            TryBindToNode(subscriber, disposable);
+            return disposable;
+        }
+
+        /// <summary>订阅指定类型的消息，并附加过滤条件。订阅会自动绑定到节点的生命周期。</summary>
+        public static IDisposable Subscribe<TMessage>(this IMessageSubscriber subscriber, Predicate<TMessage> filter, Action<TMessage> handler)
+        {
+            var disposable = _broker.Subscribe(filter).Subscribe(handler);
+            TryBindToNode(subscriber, disposable);
+            return disposable;
+        }
+
+        /// <summary>订阅指定键值的消息。订阅会自动绑定到节点的生命周期。</summary>
+        public static IDisposable Subscribe<TKey, TMessage>(this IMessageSubscriber subscriber, TKey key, Action<TMessage> handler)
+        {
+            var disposable = _broker.Subscribe<TKey, TMessage>(key).Subscribe(handler);
+            TryBindToNode(subscriber, disposable);
+            return disposable;
+        }
+
+        /// <summary>异步订阅。消息到达时执行异步处理器。订阅会自动绑定到节点的生命周期。</summary>
+        public static IDisposable SubscribeAsync<TMessage>(this IMessageSubscriber subscriber, Func<TMessage, UniTask> asyncHandler)
+        {
+            var disposable = _broker.SubscribeAsync(asyncHandler).Subscribe(_ => { });
+            TryBindToNode(subscriber, disposable);
+            return disposable;
+        }
+
+        /// <summary>异步订阅，并附加过滤条件。订阅会自动绑定到节点的生命周期。</summary>
+        public static IDisposable SubscribeAsync<TMessage>(this IMessageSubscriber subscriber, Predicate<TMessage> filter, Func<TMessage, UniTask> asyncHandler)
+        {
+            var disposable = _broker.SubscribeAsync(filter, asyncHandler).Subscribe(_ => { });
+            TryBindToNode(subscriber, disposable);
+            return disposable;
+        }
+
+        /// <summary>订阅带缓冲的消息。新订阅者会立即收到最近一次发布的消息。订阅会自动绑定到节点的生命周期。</summary>
+        public static IDisposable SubscribeBuffered<TMessage>(this IMessageSubscriber subscriber, Action<TMessage> handler)
+        {
+            var disposable = _broker.SubscribeBuffered<TMessage>().Subscribe(handler);
+            TryBindToNode(subscriber, disposable);
+            return disposable;
+        }
+
+        /// <summary>订阅带缓冲的键值消息。新订阅者会立即收到最近一次发布的消息。订阅会自动绑定到节点的生命周期。</summary>
+        public static IDisposable SubscribeBuffered<TKey, TMessage>(this IMessageSubscriber subscriber, TKey key, Action<TMessage> handler)
+        {
+            var disposable = _broker.SubscribeBuffered<TKey, TMessage>(key).Subscribe(handler);
+            TryBindToNode(subscriber, disposable);
+            return disposable;
+        }
+
+        #endregion
+
+        #region Internal
+
+        /// <summary>
+        /// 替换内部 Broker 实例（用于测试或自定义 Broker）。
+        /// </summary>
+        internal static void Replace(MessageBroker broker) => _broker = broker;
+
+        /// <summary>
+        /// 如果订阅者是 BaseNode，将订阅绑定到节点生命周期，节点销毁时自动取消。
+        /// </summary>
+        private static void TryBindToNode(object subscriber, IDisposable disposable)
+        {
+            if (subscriber is BaseNode node)
+            {
+                node.OnNodeDestroy += _ => disposable.Dispose();
+            }
+        }
+
+        #endregion
+    }
+}
